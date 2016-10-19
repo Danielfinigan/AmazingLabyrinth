3d965ba03f8c095d174dd70a41cbbfcbb833c374
Dan Finigan <danielfinigan@gmail.com>
2016-10-19 17:03:22 -0400

    Added Player script that makes the ball respond to kboard input

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private float speed;
+    public Rigidbody rb = new Rigidbody();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void FixedUpdate()
+    {
+        rb.AddForce(Input.GetAxis("Horizontal") * speed, 0, 0);
+        rb.AddForce(0, 0, Input.GetAxis("Vertical") * speed);
+    }
+}
