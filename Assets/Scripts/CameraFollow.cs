508b7d966135e3f58a73c4628ddeb977d707668e
Dan Finigan <danielfinigan@gmail.com>
2016-10-20 09:29:30 -0400

    added camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform followObject;
+    public float distance = 2f;
+    void Start()
+    {
+        Vector3 rotation = new Vector3(45, 0, 0);
+        transform.eulerAngles = rotation;
+    }
+
+    void Update()
+    {        
+        //Vector3 direction = followObject.GetComponent<Rigidbody>().velocity.normalized;
+        //Vector3 offset = distance * direction * rotation;
+        transform.position = new Vector3(0, followObject.position.y + 20, followObject.position.z - 30);
+    }
+}
