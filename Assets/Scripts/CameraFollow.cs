aa2d321ba8aa1f32eb3f10a95e293125ca9c6663
Dan Finigan <danielfinigan@gmail.com>
2016-10-20 21:35:41 -0400

    placed objects for 1 of 3 lanes in scene3

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,13 +7,13 @@ public class CameraFollow : MonoBehaviour {
     public float distance = 2f;
     void Start()
     {
-        Vector3 rotation = new Vector3(45, 0, 0);
+        Vector3 rotation = new Vector3(60, 0, 0);
         transform.eulerAngles = rotation;
-        transform.position = new Vector3(followObject.position.x, followObject.position.y + 20, followObject.position.z - 30);
+        transform.position = new Vector3(followObject.position.x, followObject.position.y + 20, followObject.position.z - 15);
     }
 
     void Update()
     {        
-        transform.position = new Vector3(followObject.position.x, followObject.position.y + 20, followObject.position.z - 30);
+        transform.position = new Vector3(followObject.position.x, followObject.position.y + 10, followObject.position.z - 5);
     }
 }
