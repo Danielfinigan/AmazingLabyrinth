0ff2881c04a1778518f9fc2a54023ea88692dff2
Dan Finigan <danielfinigan@gmail.com>
2016-10-25 14:03:57 -0400

    began work on camera twist script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,7 @@ public class CameraFollow : MonoBehaviour {
     public Transform followObject;
     void Start()
     {
+
         if (SceneManager.GetActiveScene().name == "scene3")
             transform.position = new Vector3(followObject.position.x, followObject.position.y + 10, followObject.position.z - 5);
         else
--- Assets/Scripts/CameraTwistTrigger.cs
+++ Assets/Scripts/CameraTwistTrigger.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraTwist : MonoBehaviour {
+public class CameraTwistTrigger : MonoBehaviour {
 
+    public Transform followObject;
     void OnTriggerEnter()
     {
-
+        Vector3 rotation = new Vector3(45, 90, 0);
+        transform.eulerAngles = rotation;
+        transform.position = new Vector3(followObject.position.x +5, followObject.position.y + 10, followObject.position.z);
     }
 	// Use this for initialization
 	void Start () {
