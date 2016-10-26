bc47d04fcdc6562f95c2e82a27136801c33f161d
ian <ianyoung37@yahoo.com>
2016-10-25 20:44:36 -0400

    fixed highscore UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,15 +15,17 @@ public class GameManager : MonoBehaviour {
 
 	public static GameManager instance;
 	public GameState currentGameState = GameState.menu;
+
 	public Canvas menuCanvas;  //start menu
 	public Canvas inGameCanvas;  //timer
 	public Canvas levelCompleteCanvas;  //after every complete level
 	public Canvas gameOverCanvas;  //appears when player touches kill trigger
 	public Canvas youWinCanvas;  //appears after completing third level
+	public Canvas highScoreCanvas;
 
 	public float timer;
 	public string seconds;
-    public bool newHighScore;
+	public bool newHighScore = false;
 	public static bool timeStarted = false;
 
 	public void Awake () {
@@ -34,9 +36,9 @@ public class GameManager : MonoBehaviour {
 		SetGameState (GameState.inGame);
 		Player.instance.StartGame ();
 		timeStarted = true;
-        PlayerPrefs.GetFloat("time1", 1000f);
+        /*PlayerPrefs.GetFloat("time1", 1000f);
         PlayerPrefs.GetFloat("time2", 1000f);
-        PlayerPrefs.GetFloat("time3", 3000f);
+        PlayerPrefs.GetFloat("time3", 3000f);*/
        // Debug.Log("scene1 high score = " + PlayerPrefs.GetFloat("time1"));
     }
 
@@ -45,7 +47,7 @@ public class GameManager : MonoBehaviour {
     public void LevelComplete() {
         /* if this is scene 1 and this time 
 		 * was faster than 1000 seconds...*/
-        newHighScore = false;
+        
         if (SceneManager.GetActiveScene().name == "scene1")
         {
             if (PlayerPrefs.GetFloat("time1", 0) == 0)
@@ -86,7 +88,7 @@ public class GameManager : MonoBehaviour {
                 newHighScore = true;
             }
         }
-        Debug.Log("newHighScore = " + newHighScore.ToString());
+        //Debug.Log("newHighScore = " + newHighScore.ToString());
         //Changes game state to levelComplete or YouWin
         if (SceneManager.GetActiveScene().name == "scene3")
             YouWin();
@@ -138,29 +140,38 @@ public class GameManager : MonoBehaviour {
 			levelCompleteCanvas.enabled = false;
 			gameOverCanvas.enabled = false;
 			youWinCanvas.enabled = false;
+			highScoreCanvas.enabled = false;
 		} else if (newGameState == GameState.inGame) {
 			menuCanvas.enabled = false;
 			inGameCanvas.enabled = true;
 			levelCompleteCanvas.enabled = false;
 			gameOverCanvas.enabled = false;
 			youWinCanvas.enabled = false;
+			highScoreCanvas.enabled = false;
 		} else if (newGameState == GameState.levelComplete) {
 			menuCanvas.enabled = false;
 			inGameCanvas.enabled = false;
 			levelCompleteCanvas.enabled = true;
 			youWinCanvas.enabled = false;
+			if (this.newHighScore) {
+				highScoreCanvas.enabled = true;
+			}
 		} else if (newGameState == GameState.gameOver) {
 			menuCanvas.enabled = false;
 			inGameCanvas.enabled = false;
 			levelCompleteCanvas.enabled = false;
 			gameOverCanvas.enabled = true;
 			youWinCanvas.enabled = false;
+			highScoreCanvas.enabled = false;
 		} else if (newGameState == GameState.youWin) {
 			menuCanvas.enabled = false;
 			inGameCanvas.enabled = false;
 			gameOverCanvas.enabled = false;
 			levelCompleteCanvas.enabled = false;
 			youWinCanvas.enabled = true;
+			if (this.newHighScore) {
+				highScoreCanvas.enabled = true;
+			}
 		}
 		currentGameState = newGameState;
 	}
